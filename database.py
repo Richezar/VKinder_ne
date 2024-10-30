7da2fb294ab947693fc5f7a3438a6561b73a73bd
Nikita Fomenko <fomenkoper@gmail.com>
2024-10-31 00:15:38 +0300

    first commit

---
+++ database.py
@@ -0,0 +1,17 @@
+import sqlalchemy as sq
+from sqlalchemy.orm import declarative_base, relationship
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    id_user = sq.Column(sq.String(length=20), unique=True, nullable=False)
+    age = sq.Column(sq.Integer, nullable=False)
+    sex = sq.Column(sq.String(length=7), nullable=False)
+    city = sq.Column(sq.String(length=20), nullable=False)
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
