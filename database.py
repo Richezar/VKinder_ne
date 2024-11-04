ea72ee326b86eaffb2536b6f441a079472bee11d
Nikolay Shirokov <shirokiy93@gmail.com>
2024-11-04 19:18:30 +0700

    add find_user and favorites_users

--- database.py
+++ database.py
@@ -16,7 +16,25 @@ class Users(Base):
     city = sq.Column(sq.String(length=20), nullable=False)
     id_city = sq.Column(sq.Integer, nullable=False)
 
+class Find_Users(Base):
+    """СОЗДАНИЕ ТАБЛИЦЫ USERS (НАЙДЕННЫЕ ПОЛЬЗОВАТЕЛИ)"""
+    __tablename__ = 'find_users'
 
+    id = sq.Column(sq.Integer, primary_key=True)
+    first_name_user = sq.Column(sq.String(length=20), unique=False, nullable=False)
+    last_name_user = sq.Column(sq.String(length=20), unique=False, nullable=False)
+    vk_id_user = sq.Column(sq.String(length=20), unique=True, nullable=False)
+    vk_link_user = sq.Column(sq.String(length=50), unique=True, nullable=False)
+
+class Favorites_Users(Base):
+    """СОЗДАНИЕ ТАБЛИЦЫ USERS (ИЗБРАННЫЕ ПОЛЬЗОВАТЕЛИ)"""
+    __tablename__ = 'favorites_users'
+
+    id = sq.Column(sq.Integer, primary_key=True)
+    first_name_user = sq.Column(sq.String(length=20), unique=False, nullable=False)
+    last_name_user = sq.Column(sq.String(length=20), unique=False, nullable=False)
+    vk_id_user = sq.Column(sq.String(length=20), unique=True, nullable=False)
+    vk_link_user = sq.Column(sq.String(length=50), unique=True, nullable=False)
 def create_tables(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import requests
 import vk_api
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.longpoll import VkLongPoll, VkEventType
-from database import create_tables, Users
+from database import create_tables, Users, Find_Users, Favorites_Users
 import sqlalchemy as sq
 from sqlalchemy.orm import sessionmaker
 from config import token_bot, user_token
@@ -19,10 +19,14 @@ token = token_bot
 
 vk = vk_api.VkApi(token=token)
 longpoll = VkLongPoll(vk)  # РАБОТА С СООБЩЕНИЯМИ
-
 keyboard_ontime = VkKeyboard(one_time=False)
 keyboard_ontime.add_button('найти', color=VkKeyboardColor.SECONDARY)
-keyboard_ontime.add_button('избранное', color=VkKeyboardColor.POSITIVE)
+keyboard_ontime.add_line()
+keyboard_ontime.add_button('в избранное', color=VkKeyboardColor.POSITIVE)
+keyboard_ontime.add_line()
+keyboard_ontime.add_button('в избранном', color=VkKeyboardColor.POSITIVE)
+keyboard_ontime.add_line()
+keyboard_ontime.add_button('далее', color=VkKeyboardColor.POSITIVE)
 
 
 def write_msg(user_id, message, keyboard=None):
@@ -60,6 +64,16 @@ class VkUser:
         self.id_city = self.get_id_city()
         self.age = self.get_age(response['response'][0])
         return [self.id_user, self.first_name, self.last_name, self.sex, self.city, self.id_city, self.age]
+
+    def get_sex(self):
+        url = 'https://api.vk.com/method/users.get'
+        params = {'access_token': token,
+                  'user_ids': self.id_user,
+                  'fields': 'first_name,bdate,city,sex',
+                  'v': '5.131'}
+        repl = requests.get(url, params=params)
+        response = repl.json()
+        return response['response'][0]['sex']
 
     def get_city(self, result):
         for i in result['response']:
@@ -108,6 +122,36 @@ class VkUser:
                         age = event.text
                         return age
 
+    def find_user(self):
+        """ПОИСК ЧЕЛОВЕКА ПО ПОЛУЧЕННЫМ ДАННЫМ"""
+        url = f'https://api.vk.com/method/users.search'
+        params = {'access_token': user_token,
+                  'v': '5.131',
+                  'sex': self.get_sex(),
+                  'age_from': self.age,
+                  'age_to': self.age,
+                  'city': self.id_city,
+                  'fields': 'is_closed, id, first_name, last_name',
+                  'count': 1000}
+        resp = requests.get(url, params=params)
+        resp_json = resp.json()
+        dict_1 = resp_json['response']
+        list_1 = dict_1['items']
+        for person_dict in list_1:
+            if person_dict.get('is_closed') == False:
+                self.first_name_user = person_dict.get('first_name')
+                self.last_name_user = person_dict.get('last_name')
+                self.vk_id_user = str(person_dict.get('id'))
+                self.vk_link_user = 'vk.com/id' + str(person_dict.get('id'))
+                session.add(
+                    Find_Users(first_name_user=self.first_name_user, last_name_user=self.last_name_user,
+                               vk_id_user=self.vk_id_user,
+                               vk_link_user=self.vk_link_user))
+                session.commit()
+            else:
+                continue
+        return f'Поиск завершён'
+
 
 for event in longpoll.listen():
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
@@ -116,12 +160,45 @@ for event in longpoll.listen():
             user1 = VkUser(event.user_id)
             id_user, first_name, last_name, sex, city, id_city, age = user1.get_user_info()
             write_msg(event.user_id, f"Хай, {first_name}, id - {id_user}\n"
-                                     f"Ваш город - {city}, id - {id_city}\n"
+                                     f"Ваш город - {city}\n"
                                      f"Ваш пол - {sex}\n"
-                                     f"Ваш возраст - {age}\n", keyboard=keyboard_ontime)
-            session.add(Users(id_user=id_user, first_name=first_name, last_name=last_name, age=age, sex=sex, city=city, id_city = id_city))
+                                     f"Ваш возраст - {age}\n\n"
+                                     f"Нажми кнопку 'найти' и я выведу найденного человека", keyboard=keyboard_ontime)
+            session.add(Users(id_user=id_user, first_name=first_name, last_name=last_name, age=age, sex=sex, city=city,
+                              id_city=id_city))
             session.commit()
-        elif request == "пока":
-            write_msg(event.user_id, "Пока((")
+            user1.find_user()
+        elif request == "найти":
+            first = session.query(Find_Users).first()
+            write_msg(event.user_id,
+                      f'- {first.first_name_user} {first.last_name_user}\n'
+                      f'- {first.vk_link_user}\n'
+                      f'три фотографии')
+        elif request == "далее":
+            try:
+                session.delete(first)
+                session.commit()
+                first = session.query(Find_Users).first()
+                write_msg(event.user_id,
+                          f'- {first.first_name_user} {first.last_name_user}\n'
+                          f'- {first.vk_link_user}\n'
+                          f'три фотографии')
+            except AttributeError:
+                write_msg(event.user_id, f'Людей больше не найдено')
+        elif request == "в избранное":
+            favorite = Favorites_Users(first_name_user=first.first_name_user, last_name_user=first.last_name_user,
+                                       vk_id_user=first.vk_id_user, vk_link_user=first.vk_link_user)
+            session.add(favorite)  # добавляем в бд
+            session.commit()  # сохраняем изменения
+            session.refresh(favorite)  # обновляем состояние объекта
+            write_msg(event.user_id,
+                      f'- {first.first_name_user} {first.last_name_user} добавлен(а) в избранное\n')
+        elif request == "в избранном":
+            favorites_users = session.query(Favorites_Users).all()
+            for f_user in favorites_users:
+                write_msg(event.user_id,
+                          f'- {f_user.first_name_user} {f_user.last_name_user}\n'
+                          f'- {f_user.vk_link_user}\n'
+                          f'три фотографии')
         else:
             write_msg(event.user_id, "Не поняла вашего ответа...")
